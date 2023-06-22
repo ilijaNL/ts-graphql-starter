2bdd9cd79226e9040c26a1811e812500475857a6
ilijaNL <ilija.lisenkov@gmail.com>
2023-06-22 18:24:16 +0200

    Add hive usage analytics

--- apps/webapp/codegen.ts
+++ apps/webapp/codegen.ts
@@ -1,4 +1,8 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
+import createListFn from 'graphql-codegen-on-operations/lib/use/listing';
+import { GenerateFn } from 'graphql-codegen-on-operations';
+import { createCollector, OperationDefinition } from 'graphql-hive-edge-client';
+
 const sharedConfig = {
   enumsAsTypes: true,
   skipTypename: true,
@@ -24,6 +28,20 @@ const sharedConfig = {
   },
 };
 
+const hiveOpsGen: GenerateFn = (schema, { documents }) => {
+  const collect = createCollector(schema);
+  // generate record<operationName, item>
+  const result = documents.reduce((agg, curr) => {
+    if (curr.operation.name) {
+      agg[curr.operation.name] = collect(curr.node, null);
+    }
+
+    return agg;
+  }, {} as Record<string, OperationDefinition>);
+
+  return JSON.stringify(result, null, 2);
+};
+
 const config: CodegenConfig = {
   schema: [
     {
@@ -46,7 +64,16 @@ const config: CodegenConfig = {
       },
     },
     './src/__generated__/operations.json': {
-      plugins: ['graphql-operation-list'],
+      plugins: ['graphql-codegen-on-operations'],
+      config: {
+        gen: createListFn(),
+      },
+    },
+    './src/__generated__/hive-ops.json': {
+      plugins: ['graphql-codegen-on-operations'],
+      config: {
+        gen: hiveOpsGen,
+      },
     },
   },
 };
--- apps/webapp/src/pages/api/g.ts
+++ apps/webapp/src/pages/api/g.ts
@@ -1,60 +1,91 @@
-import { createReportHooks, createReport, ReportContext } from '@graphql-edge/proxy/lib/reporting';
-import {
-  GeneratedOperation,
-  OpsDef,
-  createOperationParseFn,
-  createOperationStore,
-} from '@graphql-edge/proxy/lib/operations';
+import { createErrorResponse, createGraphQLProxy, createParseErrorResponse, isParsedError } from '@graphql-edge/proxy';
+import { GeneratedOperation, createOperationParseFn, createOperationStore } from '@graphql-edge/proxy/lib/operations';
 import OperationList from '../../__generated__/operations.json';
+import OperationHiveMap from '../../__generated__/hive-ops.json';
 import { NextFetchEvent, NextRequest } from 'next/server';
-import { createHandler, isParsedError } from '@graphql-edge/proxy';
+import { OperationDefinition, createHiveSendFn, createUsageCollector } from 'graphql-hive-edge-client';
 
 const store = createOperationStore(OperationList as Array<GeneratedOperation>);
 
 export const config = {
   runtime: 'edge',
+  unstable_allowDynamic: [
+    // use a glob to allow anything in the function-bind 3rd party module
+    '**/.pnpm/**/node_modules/lodash*/*.js',
+  ],
 };
 
-const reportHooks = createReportHooks();
+const proxy = createGraphQLProxy(process.env.GRAPHQL_ENDPOINT!, createOperationParseFn(store));
 
-type Context = ReportContext & { def: OpsDef | null };
-
-const handler = createHandler(process.env.GRAPHQL_ENDPOINT!, createOperationParseFn(store), {
-  hooks: {
-    // use hook to assign data from parsed request to the context, which will be used for caching
-    onRequestParsed(parsed, ctx: Context) {
-      if (!isParsedError(parsed)) {
-        ctx.def = parsed.def;
-      }
-      reportHooks.onRequestParsed(parsed, ctx);
-    },
-    onProxied: reportHooks.onProxied,
-    onResponseParsed: reportHooks.onResponseParsed,
-  },
+const usageCollector = createUsageCollector({
+  maxSize: 25,
+  send: createHiveSendFn(process.env.GRAPHQL_HIVE_TOKEN!),
+  sendInterval: 5_000,
+  sampleRate: 1.0,
 });
 
-export default async function MyEdgeFunction(request: NextRequest, event: NextFetchEvent) {
-  event.passThroughOnException();
-  const report = createReport();
+const handle = async (request: Request, event: NextFetchEvent) => {
+  const parsed = await proxy.parseRequest(request);
 
-  // this is mutable object
-  const context: Context = Object.assign(report.context, {
-    def: null,
-  });
-  const response = await handler(request, context);
+  if (isParsedError(parsed)) {
+    return createParseErrorResponse(parsed);
+  }
+  const hiveOp = parsed.operationName
+    ? (OperationHiveMap as Record<string, OperationDefinition>)[parsed.operationName]
+    : undefined;
 
-  const cacheTTL = context.def?.behaviour.ttl;
+  const finish =
+    hiveOp &&
+    usageCollector.collect(hiveOp, {
+      name: 'ts-starter-template',
+      version: '0.0.1',
+    });
+
+  const proxyResponse = await proxy.proxy(parsed);
+  if (!proxyResponse.ok) {
+    finish &&
+      event.waitUntil(
+        finish({
+          ok: false,
+          errors: [{ message: 'cannot fetch' }],
+        })
+      );
+    return proxyResponse;
+  }
+
+  const parsedResult = await proxy.parseResponse(proxyResponse);
+
+  if (!parsedResult) {
+    finish &&
+      event.waitUntil(
+        finish({
+          ok: false,
+          errors: [{ message: 'cannot parse' }],
+        })
+      );
+    return createErrorResponse('cannot parse response', 406);
+  }
+
+  finish &&
+    event.waitUntil(
+      finish({
+        ok: true,
+        errors: parsedResult.errors,
+      })
+    );
+
+  const response = await proxy.formatResponse(parsedResult, proxyResponse);
+
+  const cacheTTL = parsed.def?.behaviour.ttl;
 
   if (cacheTTL) {
     response.headers.set('Cache-Control', `public, s-maxage=${cacheTTL}, stale-while-revalidate=${cacheTTL}`);
   }
 
-  event.waitUntil(
-    report.collect(response, context).then((report) => {
-      // eslint-disable-next-line no-console
-      report && console.log({ report: JSON.stringify(report, null, 2) });
-    })
-  );
-
   return response;
+};
+
+export default async function MyEdgeFunction(request: NextRequest, event: NextFetchEvent) {
+  event.passThroughOnException();
+  return handle(request, event);
 }
